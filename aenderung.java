564241a3df70db3d37a0b826bbe3e69118647d8a
Alexander <alexander.albrecht2004@gmail.com>
2025-05-29 19:52:49 +0200

    Version 2

--- aenderung.java
+++ aenderung.java
@@ -1,5 +1,5 @@
 public class aenderung {
     public static void main(String[] args){
-        System.out.println("Hall0 Welt!");
+        System.out.println("Hall0 Welt, wie gehts heute?");
     }
 }
