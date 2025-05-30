c4fb6b4129b3b1fde114435b4dac6ab96c96a590
Alexander <alexander.albrecht2004@gmail.com>
2025-05-30 21:23:46 +0200

    Version 33

--- aenderung.java
+++ aenderung.java
@@ -1,5 +1,6 @@
 public class aenderung {
     public static void main(String[] args){
-        System.out.println("Hallo Welt!");
+        System.out.println("Hallo Welt, wie gehts heute?");
+        System.out.println("Änderung 33");
     }
 }
